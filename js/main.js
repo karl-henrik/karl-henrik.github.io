feaebefe6769d7ffceef076fd379c2245fcbb530
karl-henrik <karl-henrik@mean.se>
2017-08-27 22:43:40 +0200

    Fixed broken script after ruby update

--- js/main.js
+++ js/main.js
@@ -1,87 +1,84 @@
--- -
-layout: null
-    -- -
-    $(document).ready(function() {
-        $('a.blog-button').click(function(e) {
-            $(".content-wrapper").html(getContentFromURL("{{site.url}}"));
-            setAnimation();
-        })
+   $(document).ready(function() {
+       $('a.blog-button').click(function(e) {
+           $(".content-wrapper").html(getContentFromURL("{{site.url}}"));
+           setAnimation();
+       })
 
-        $('a.about-button').click(function(e) {
-            $(".content-wrapper").html(getContentFromURL("{{site.url}}about"));
-            setAnimation();
-        })
+       $('a.about-button').click(function(e) {
+           $(".content-wrapper").html(getContentFromURL("{{site.url}}about"));
+           setAnimation();
+       })
 
-        $('a.speaking-button').click(function(e) {
-            $(".content-wrapper").html(getContentFromURL("{{site.url}}speaking"));
-            setAnimation();
-        })
+       $('a.speaking-button').click(function(e) {
+           $(".content-wrapper").html(getContentFromURL("{{site.url}}speaking"));
+           setAnimation();
+       })
 
-        if (window.location.hash && window.location.hash == '#about') {
-            $(".content-wrapper").html(getContentFromURL("{{ site.url}}about"));
-            $('.panel-cover').addClass('panel-cover--collapsed')
-        }
+       if (window.location.hash && window.location.hash == '#about') {
+           $(".content-wrapper").html(getContentFromURL("{{ site.url}}about"));
+           $('.panel-cover').addClass('panel-cover--collapsed')
+       }
 
-        if (window.location.hash && window.location.hash == '#blog') {
-            $(".content-wrapper").html(getContentFromURL("{{ site.url}}"));
-            $('.panel-cover').addClass('panel-cover--collapsed')
-        }
+       if (window.location.hash && window.location.hash == '#blog') {
+           $(".content-wrapper").html(getContentFromURL("{{ site.url}}"));
+           $('.panel-cover').addClass('panel-cover--collapsed')
+       }
 
-        if (window.location.hash && window.location.hash == '#speaking') {
-            $(".content-wrapper").html(getContentFromURL("{{ site.url}}speaking"));
-            $('.panel-cover').addClass('panel-cover--collapsed')
-        }
+       if (window.location.hash && window.location.hash == '#speaking') {
+           $(".content-wrapper").html(getContentFromURL("{{ site.url}}speaking"));
+           $('.panel-cover').addClass('panel-cover--collapsed')
+       }
 
-        if (window.location.pathname !== '{{ site.url }}' && window.location.pathname !== '{{ site.url }}index.html') {
-            $('.panel-cover').addClass('panel-cover--collapsed')
-        }
+       if (window.location.pathname !== '{{ site.url }}' && window.location.pathname !== '{{ site.url }}index.html') {
+           $('.panel-cover').addClass('panel-cover--collapsed')
+       }
 
 
-        $('.btn-mobile-menu').click(function() {
-            $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
-            $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
-        })
+       $('.btn-mobile-menu').click(function() {
+           $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
+           $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+       })
 
-        $('.navigation-wrapper .blog-button').click(function() {
-            $('.navigation-wrapper').toggleClass('visible')
-            $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
-        })
+       $('.navigation-wrapper .blog-button').click(function() {
+           $('.navigation-wrapper').toggleClass('visible')
+           $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+       })
 
-    })
+   })
 
-var getContentFromURL = function(url) {
-    var result = null;
+   var getContentFromURL = function(url) {
+       var result = null;
 
-    $.ajax({
-        url: url,
-        type: 'get',
-        dataType: 'html',
-        async: false,
-        success: function(data) {
-            result = data;
-        }
-    });
+       $.ajax({
+           url: url,
+           type: 'get',
+           dataType: 'html',
+           async: false,
+           success: function(data) {
+               result = data;
+           }
+       });
 
-    var elements = $(result);
-    var found_content = $(".content-wrapper__inner", elements);
+       var elements = $(result);
+       var found_content = $(".content-wrapper__inner", elements);
 
-    return found_content;
+       return found_content;
 
-}
+   }
 
-var setAnimation = function() {
-    if ($('.panel-cover').hasClass('panel-cover--collapsed')) {
-        return
+   var setAnimation = function() {
+       if ($('.panel-cover').hasClass('panel-cover--collapsed')) {
+           return
 
-    }
-    currentWidth = $('.panel-cover').width()
-    if (currentWidth < 960) {
-        $('.panel-cover').addClass('panel-cover--collapsed')
-        $('.content-wrapper').addClass('animated slideInRight')
-    } else {
-        $('.panel-cover').addClass('panel-cover--collapsed')
-        $('.panel-cover').css('max-width', currentWidth)
-        $('.panel-cover').animate({ 'max-width': '530px', 'width': '40%' }, 400, swing = 'swing', function() {})
+       }
+       currentWidth = $('.panel-cover').width()
+       if (currentWidth < 960) {
+           $('.panel-cover').addClass('panel-cover--collapsed')
+           $('.content-wrapper').addClass('animated slideInRight')
+       } else {
+           $('.panel-cover').addClass('panel-cover--collapsed')
+           $('.panel-cover').css('max-width', currentWidth)
+           $('.panel-cover').animate({ 'max-width': '530px', 'width': '40%' }, 400, swing = 'swing', function() {})
 
-    }
-}
+       }
+   }
