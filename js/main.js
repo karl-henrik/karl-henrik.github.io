2b4e79b93eb4c45b7277f58db972100794de9226
karl-henrik <karl-henrik@mean.se>
2017-08-28 13:56:35 +0200

    debugging

--- js/main.js
+++ js/main.js
@@ -32,9 +32,9 @@ layout: null
             $('.panel-cover').addClass('panel-cover--collapsed')
         }
 
-        if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
-            $('.panel-cover').addClass('panel-cover--collapsed')
-        }
+        // if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
+        //     $('.panel-cover').addClass('panel-cover--collapsed')
+        // }
 
 
         $('.btn-mobile-menu').click(function() {
